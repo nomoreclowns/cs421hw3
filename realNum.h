ecc980c91e1aa5af5828fe9372a3afca785fe2a4
xcatz <richardluu2003@gmail.com>
2014-03-09 18:33:19 -0700

    panzers

---
+++ realNum.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class realNum
+{
+	protected:
+
+		string user;
+	
+	public:
+	
+		realNum();	
+		bool realNumber(const string &);
+		bool checkTheNumber(const char &);
+};
