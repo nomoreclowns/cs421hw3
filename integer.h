2e14a3e2ad28e4c71d609504750db52ead46c08f
xcatz <richardluu2003@gmail.com>
2014-03-10 18:59:27 -0700

    Create integer.h

---
+++ integer.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class integer
+{
+protected:
+
+	string intNum;
+
+public:
+
+	integer();
+	bool integer_token(const string &);
+	
+};
